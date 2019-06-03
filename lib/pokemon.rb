37609700c7fbdd39b84b7843675928506ee5a6bf
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 03:40:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,1 +1,12 @@
-"CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);"
+def self.create_table
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS pokemon(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT
+  )
+  SQL
+
+  DB[:conn].execute(sql)
+end
+    
