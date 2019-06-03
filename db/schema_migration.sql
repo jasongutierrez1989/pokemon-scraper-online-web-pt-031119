9a9bc70ad0c51e213463ff65b70801c43ddf7531
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 03:50:14 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,16 +1,7 @@
--- Write your sql commands to create your db schema here.
--- It should create a table in a new table in the db/ directory.
-def self.create_table
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS pokemon(
-      id PRIMARY KEY INTEGER,
-      name TEXT,
-      type TEXT,
-      db TEXT,
-      hp TEXT
-    )
-    SQL
-
-    DB[:conn].execute(sql)
-
-end
+CREATE TABLE IF NOT EXISTS pokemon(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  type TEXT,
+  db TEXT,
+  hp TEXT
+);
