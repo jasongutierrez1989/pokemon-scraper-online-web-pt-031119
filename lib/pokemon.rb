bc7e5b033515d1090b4c813339572146faefbcdd
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 03:41:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,26 @@
-def self.create_table
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS pokemon(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    type TEXT
-  )
-  SQL
+class Pokemon
+  attr_accessor :name, :type, :db, :id, :hp
 
-  DB[:conn].execute(sql)
+  def initialize(keywords)
+
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(num, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
+    new_pokemon = self.new(pokemon)
+    new_pokemon.id = pokemon[0][0]
+    new_pokemon.name = pokemon[0][1]
+    new_pokemon.type = pokemon[0][2]
+    new_pokemon.hp = pokemon[0][3]
+    return new_pokemon
+  end
+
+  def alter_hp(num, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
+    self.hp = num
+  end
 end
