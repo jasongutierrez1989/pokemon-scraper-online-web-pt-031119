e9381a706ae4ba15e309c070fa292ae6aa5e41d6
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 04:11:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,6 +10,7 @@ class Pokemon
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+    db.execute(self.name, self.type)
   end
 
   def self.find(num, db)
