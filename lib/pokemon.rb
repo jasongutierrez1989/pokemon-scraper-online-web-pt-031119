e0b6c21043680ed9ac3bdf1974c82e22271e64cc
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-03 03:28:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,11 @@ class Pokemon
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
 
-  def initialize(id = nil, name, type, hp)
+  def initialize(id = nil, name, type, db, hp)
     @id = id
     @name = name
     @type = type
+    @db = db
     @hp = hp
   end
 
